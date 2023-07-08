4cc663cab98ea0d6033573d8578e1ae119bb42e6
Darwin <darwinvinicio14_11@hotmail.com>
2023-07-07 22:36:54 -0500

    Inicio del proyecto Darwin

---
+++ TutorialCrearProyecto/src/main/java/com/ec/ecuador/TutorialCrearProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.ec.ecuador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialCrearProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialCrearProyectoApplication.class, args);
+	}
+
+}
